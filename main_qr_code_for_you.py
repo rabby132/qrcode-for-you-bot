a4ba0b5aac8a8616cf285ef662615546d0cf4f10
Easin Alam Rabby <122211336+rabby132@users.noreply.github.com>
2024-01-24 22:00:10 +0600

    Update main_qr_code_for_you.py

--- main_qr_code_for_you.py
+++ main_qr_code_for_you.py
@@ -2,18 +2,18 @@ from needed_modules import *
 from functions import *
 from log import *
 
-bot = telebot.TeleBot(bot_api)
+bot = telebot.TeleBot(6836264981:AAGd2EPZXoHuZK4B5TxjcgbcwgBnFUtE8w8)
 
 @bot.message_handler(commands=['start'])
 def start_sent(message):
-    log(f"{bot_name} log:\n\nstart command received from user {message.chat.id}")
+    log(f"{Dogbot Dwonloder} log:\n\nstart command received from user {message.chat.id}")
     bot.send_message(message.chat.id, \
                         start_cmd_msg, \
                         disable_web_page_preview=True)
 
 @bot.message_handler(commands=['help'])
 def help_sent(message):
-    log(f"{bot_name} log:\n\nhelp command received from user {message.chat.id}")
+    log(f"{Dogbot Dwonloder} log:\n\nhelp command received from user {message.chat.id}")
     bot.send_message(message.chat.id, \
                         help_cmd_msg, \
                         disable_web_page_preview=True)
